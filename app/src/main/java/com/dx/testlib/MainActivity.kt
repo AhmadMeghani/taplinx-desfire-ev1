fc4d2bdc492c2018e396171befc70a75b0743770
dexter <dexter@newlogic.com>
2021-04-21 15:09:56 +0800

    Add initial NFC scaffolding

--- app/src/main/java/com/dx/testlib/MainActivity.kt
+++ app/src/main/java/com/dx/testlib/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.dx.testlib
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nfc: NfcAdapter
+    var nfcPendingIntent: PendingIntent? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nfc = NfcAdapter.getDefaultAdapter(getApplicationContext())
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfc.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfc.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
     }
 }
